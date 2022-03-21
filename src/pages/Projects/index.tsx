3efd09d6329507cf87650ff7116b2382b0bfb66e
EdreyC <edreycarneiro.profissional@gmail.com>
2022-03-21 13:12:59 -0300

    projects UI update

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -1,36 +1,38 @@
 import { useQuery } from "react-query";
 import axios from 'axios';
-import {BiLinkAlt} from "react-icons/bi"
+import { BiLinkAlt } from "react-icons/bi"
 import { useContext } from "react";
 import { RepositoryContext } from './../../contexts/RepositoryContext';
 
 
 export default function Projects() {
-    
-    const {data,isFetching} = useContext(RepositoryContext)
-   
+
+    const { data, isFetching } = useContext(RepositoryContext)
+
     return (
-        <div className="flex justify-center p-5 md:p-20">
-            <ul  className=" columns-1 gap-10  md:columns-2 sm:columns-1">
+        <div className="flex items-center justify-center flex-col gap-12 md:gap-20 p-20">
+
+                <h1 className="text-4xl  font-black text-center ">Projetos</h1>
+                <ul className="  grid grid-cols-1 xl:grid-cols-6 md:grid-cols-3 sm:grid-cols-2">
                 {isFetching && <p>Carregando...</p>}
                 {
                     data?.map(repo => {
-                        let reposName =  repo.name[0].toUpperCase() + repo.name.slice(1)
-                        if(repo.description === null){
+                        let reposName = repo.name[0].toUpperCase() + repo.name.slice(1)
+                        if (repo.description === null) {
                             repo.description = "Sem descrição"
                         }
                         return (
-                            <li className="p-4 flex  flex-col"key={repo.name}>
-                                <a href={repo.html_url} className="font-bold gap-3 pl-2 pr-3 py-1 flex items-center text-xl  md:w-max hover:bg-indigo-700 rounded hover:underline hover:text-white">
-                                <BiLinkAlt className="text-2xl md:text-2xl"/>
-                              {reposName}
-                              </a>
-                             
-                               
+                            <li className="p-4 flex flex-col" key={repo.name}>
+                                <a href={repo.html_url} className=" font-bold gap-3 pl-2 pr-3 py-1 flex items-center text-xl 
+                                
+                                hover:bg-indigo-700 rounded hover:underline hover:text-white">
+                                    <BiLinkAlt className="text-2xl" />
+                                    {reposName}
+                                </a>
                                 <p className=" p-3 text-base">{repo.description}</p>
                             </li>
 
-                        ) 
+                        )
                     })}
             </ul>
         </div>
