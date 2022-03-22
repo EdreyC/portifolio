731dbb076a43e643ef0feab425e4099b522317da
EdreyC <edreycarneiro.profissional@gmail.com>
2022-03-22 01:41:01 -0300

    update

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -1,5 +1,4 @@
-import { useQuery } from "react-query";
-import axios from 'axios';
+
 import { BiLinkAlt } from "react-icons/bi"
 import { useContext } from "react";
 import { RepositoryContext } from './../../contexts/RepositoryContext';
