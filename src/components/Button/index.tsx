6fe615e622ccf0d7a00e9ed6f62471409dfbc73b
EdreyC <edreycarneiro.profissional@gmail.com>
2022-03-30 00:32:59 -0300

    color text changed

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -40,7 +40,7 @@ export function ButtonNavigateOutline(props: ButtonNavigateProps) {
 
     return (
         <Link to={props.to}>
-             <div className="flex items-center flex-row  text-indigo-800 gap-3 p-3 rounded bg-none border-2 border-indigo-800">
+             <div className="flex items-center flex-row  text-slate-800  gap-3 p-3 rounded bg-none border-2 border-indigo-800">
                {props.children}
             </div>
  
@@ -64,7 +64,7 @@ export function ButtonLinkCircle(props: ButtonLinkProps) {
 
     return (
         <a href={props.link}>
-             <div className="flex items-center flex-row bg-none text-indigo-800 border-slate-200 border dark:bg-indigo-80 dark:border-none dark:text-slate-50 gap-3 p-2 rounded-full">
+             <div className="flex items-center flex-row bg-none text-slate-800  border-slate-200 border dark:bg-indigo-80 dark:border-none dark:text-slate-50 gap-3 p-2 rounded-full">
                {props.children}
             </div>
  
